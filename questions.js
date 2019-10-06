e8873ecd8d9b00e69ba06ae15ed57dfb0ef1ed60
mariarosadelatex <mariarosadelatex@gmail.com>
2019-10-06 12:27:24 -0600

    created choiceDisplay , answerDisplay and choiceDisplay

--- questions.js
+++ questions.js
@@ -2,32 +2,45 @@
 var questions = [
     {
         tittle: "Question",
-        choices: ["a","b","c"],
-        answer: "b"
+        choices: ["a","b","c","d"],
+        answer: ["Correct","Incorrect"]
+    
     },
     {
         tittle: "Question2",
-        choices: ["a","b","c"],
+        choices: ["r","4","u","e"],
         answer: "c"
     },
 
     {
         tittle: "Question3",
-        choices: ["a","b","c"],
+        choices: ["a","b","c","d"],
         answer: "c"
     },
     {
         tittle: "Question4",
-        choices: ["a","b","c"],
+        choices: ["a","b","c","d"],
         answer: "c"
     },
     {
         tittle: "Question5",
-        choices: ["a","b","c"],
+        choices: ["a","b","c","d"],
         answer: "c"
     },
 
 ]
+
+var verification = ["Correct","Incorrect"]
+
+var questionRecap = function() {
+    if (questions[0].choices[1]) {
+       answerDisplay.textContent = questions[0].answer[0]
+    }
+    else if (questions[0].choices[0] || questions[0].choices[2] || questions[0].choices[3]) { 
+        answerDisplay.textContent = questions[0].answer[1]
+    }
+
+}
 
 
 
--- script.js
+++ script.js
@@ -2,16 +2,21 @@
 
 var startButton = document.querySelector("#start");
 var timeLeft = document.querySelector("#seconds");
-var stopButton = document.querySelector("#stop")
+var stopButton = document.querySelector("#stop");
+var questionaireDisplay = document.querySelector("#questionDisplay");
+var choiceDisplay = document.querySelector("#choiceDisplay");
+var answerDisplay = document.querySelector("#answerDisplay");
+var answerBtn = document.createElement("button");
+var btns = document.getElementsByClassName("btn");
 
 
 
 var totalSeconds = 75;
 var secondsElapsed = 0;
 var interval;
-var questions = 0;
+var questionCount = 0;
 
-
+var timerStarted = false;
 
 //This is the way the numbers will be displayed on the browser
 
@@ -33,45 +38,100 @@ var questions = 0;
 
 
 //setting my timer
-var timer = function() {
-    
-    interval = setInterval(function() {
-      secondsElapsed++;
-      showTime();
+
+var timer = function () {
+
+    interval = setInterval(function () {
+        secondsElapsed++;
+        showTime();
     }, 1000);
-  }
-  var showTime = function() {
+}
+var showTime = function () {
     var secondsLeft = totalSeconds - secondsElapsed;
     timeLeft.textContent = secondsLeft;
-    if (secondsLeft === 0) {stop();}
+    if (secondsLeft === 0) { stop(); }
 }
 
 
-  var start = function(){
+var start = function () {
+    timerStarted = true;
     timer();
-    
-    if (questions < 5) {
-    questions ++}
-    
-    else {
-        stop();}
+    console.log(questions)
+    questionaire();
+
+    // if (questions < 5) {
+    // questions ++}
+
+    // else {
+    //     stop();}
 }
-  //starting my countdown by assigning a function to showTime
-  
- 
-  var stop = function() {
-      console.log("stop")
-      clearInterval(interval)
-  }
-      
-  
-  
- 
+//Function that displays the question
+function questionaire() {
+    if (questionCount < 5) {
+        // for (var j=0; j<questions.length; j++){
+        //     var quest = document.createElement("h3");
+        //     quest.textContent = questions[j].tittle;
+        //     questionaireDisplay.appendChild(quest);
+        questionaireDisplay.textContent = questions[questionCount].tittle;
+        console.log(questionCount)
+        //clear the previous answer buttons
+        choiceDisplay.innerHTML = "";
+        for (var i = 0; i < 4; i++) {
+
+            var answerBtn = document.createElement("button");
+            answerBtn.textContent = questions[questionCount].choices[i];
+            answerBtn.setAttribute("class", "btn");
+            answerBtn.addEventListener("click", function () {
+                console.log(answerBtn)
+                questionCount++;
+                questionaire();
+                questionRecap();
+            })
+            choiceDisplay.appendChild(answerBtn);
+            
+       
 
 
-  startButton.addEventListener("click", start);
+            
+
+        }
+    } else {
+        stop();
+    }
+}
+
+//starting my countdown by assigning a function to showTime
+
+
+var stop = function () {
+    console.log("stop");
+    clearInterval(interval);
+}
 
 
 
 
 
+
+startButton.addEventListener("click", function () {
+    console.log("btn btn press")
+    if (timerStarted === false) {
+        start();
+        console.log("listener true")
+        questionaire();
+        questionRecap();
+    }
+    
+
+    //     var mybuttons =  document.getElementsByClassName('btn')
+    //     for(var i=0;i<mybuttons.length;i++){
+    //         mybuttons[i].addEventListener("click", function() {
+    //         console.log(answerBtn)
+
+    //         questionCount++;
+    //         questionaire();
+    //   } )
+
+
+
+});
